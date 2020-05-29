d9635d8611d666e39911b7b63412070f9472549f
Ruth-P <65608317+Ruth-P@users.noreply.github.com>
2020-05-29 17:25:40 +0200

    Add files via upload

---
+++ Preprocessing_ThesisRP.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import gzip
+import os 
+import tarfile
+import math
+import argparse
+import xlrd
+import csv
+import numpy as np
+import pandas as pd
+import Levenshtein as lev
+import seaborn as sns
+from collections import defaultdict, namedtuple
+from random import shuffle, uniform
+from io import open
+from future.builtins import range
+from future.utils import iteritems
+from sklearn import preprocessing
+
+## Function to Load Data and Extract Features: Returns 3D Array with : User, Format, Token, PoS and Mistake(0 or 1) per Instance. 
+def load_data(filename):
+    data = []
+    instance_properties = dict() 
+    with open(filename, 'rt', encoding = 'utf-8') as f:
+        for line in f:
+            line = line.strip()           
+            if len(line) == 0:                              # empty line means end of exercise
+                instance_properties = dict()
+            elif line[0] == '#':                             # line starts with #, means new exercise
+                if 'prompt' in line:
+                    pass
+                else:
+                    list_of_exercise_parameters = line[2:].split()
+                    for exercise_parameter in list_of_exercise_parameters:
+                        [key, value] = exercise_parameter.split(':')
+                        if key == 'user':
+                            instance_properties[key] = value
+                        elif  key == 'format':
+                            instance_properties[key] = value   
+            else:                            ## new Instance for the current exercise
+                line = line.split()             
+                instance_properties['token'] = str.lower(line[1])
+                instance_properties['part_of_speech'] = line[2]                      
+                instance_properties['mistake'] = float(line[6])
+                data.append(list(instance_properties.items()))       
+        return np.asarray(data)
+
+## Load data and extract features per track
+en_es = load_data("en_es.slam.20190204.train")
+es_en = load_data("es_en.slam.20190204.train")
+fr_en = load_data("fr_en.slam.20190204.train")
+
+## Create 2 dictionaries: 1) Number of Mistakes per User, per Word, per Format (Nr_Mistakes) 
+## 2) Number of times word was seen, per user per Format (Word_exp) . 
+
+def mistake_wordexp_dct(data):
+    uwf_mistakes=defaultdict(int)
+    word_exp=defaultdict(int)
+    
+    for i in range(data.shape[0]):
+        uwf_mistakes[(data[i][0][1]),(data[i][2][1]), (data[i][1][1]), (data[i][3][1])] += int(float((data[i][-1][1])))
+        word_exp[(data[i][0][1]),(data[i][2][1]), (data[i][1][1]), (data[i][3][1])] +=1
+    return uwf_mistakes, word_exp 
+    
+## Create a dictionary that maps English words to Concreteness and Frequency score
+Brysbaert=pd.read_excel('BrysbaertConcretenessFrequency.xlsx')
+Brysbaert=np.array(Brysbaert)
+En_freq=pd.read_excel('SUBTLEX_En.xlsx')
+En_freq=np.array(En_freq)
+
+def dct_Brys_Enfreq(brysbaert, en_freq):
+    dct_concr={}
+    dct_freq={}
+    for line in brysbaert:
+        dct_concr[line[0]]=line[2]   ## first concreteness,
+    for line in en_freq:
+        dct_freq[line[0]] = line[5]    
+    return dct_concr, dct_freq
+
+## Create Spanish frequency dictionary
+Spanish_freq=pd.read_excel('SUBTLEX-ESP.xlsx')
+Spanish_freq=np.array(Spanish_freq)
+
+def dct_Spanish(Spanish_freq):
+    dct={}
+    for line in Spanish_freq:
+        dct[line[0]] = line[2]
+        dct[line[5]] = line[7]
+        dct[line[10]] = line[12]
+    return dct
+
+## Function to add ftrs Word_exp, Language track, Word length and Nr_Mistakes. 
+def ftrs_2(dct_mistakes, dct_exp, track):
+    extd_ftrs=np.empty((len(dct_mistakes), 12), dtype=object)        ## empty array to contain all features. 
+    mist_keys=np.asarray(list(dct_mistakes))
+    exp_keys=np.asarray(list(dct_exp))
+    mist_values=np.asarray(list(dct_mistakes.values()))
+    exp_values=np.asarray(list(dct_exp.values()))
+
+    for i in range(len(extd_ftrs)):
+        extd_ftrs[i,0:4]=(mist_keys[i])
+        extd_ftrs[i,-1]=mist_values[i]
+        extd_ftrs[i,5]=track
+        extd_ftrs[i,6]=exp_values[i]
+    for j in extd_ftrs:
+        j[4]=len(str(j[1]))       ## word length 
+    return extd_ftrs
+
+## Define dictionaries 
+dct_mistakes_en_es, dct_exp_en_es = mistake_wordexp_dct(en_es)
+dct_mistakes_es_en, dct_exp_es_en = mistake_wordexp_dct(es_en)
+dct_mistakes_fr_en, dct_exp_fr_en = mistake_wordexp_dct(fr_en)
+
+dct_Brysbaert, dct_Enfreq=dct_Brys_Enfreq(Brysbaert, En_freq)
+dct_Spanish_freq=dct_Spanish(Spanish_freq)
+
+## Extend ftrs per track (Word_exp, Word_length, Track, and Nr_Mistakes)
+en_es_2 = ftrs_2(dct_mistakes_en_es, dct_exp_en_es, 'en_es')
+es_en_2 = ftrs_2(dct_mistakes_es_en, dct_exp_es_en, 'es_en')
+fr_en_2 = ftrs_2(dct_mistakes_fr_en, dct_exp_fr_en, 'fr_en')
+
+## Load the excel files with translation
+en_es_trans=pd.read_excel('en_sp_words.xlsx')
+en_es_trans=np.array(en_es_trans)
+
+es_en_trans=pd.read_excel('es_en_words.xlsx')
+es_en_trans=np.array(es_en_trans)
+
+fr_en_trans=pd.read_excel('fr_en_words.xlsx')
+fr_en_trans=np.array(fr_en_trans)
+
+## Function for creating Translation Dictionary
+def trans_dct(trans_array):
+    dct = dict()
+    for i in trans_array:
+        dct[i[0]] = i[1]
+    return dct
+
+## Define translation dictionaries for each track
+en_es_dct = trans_dct(en_es_trans)
+es_en_dct = trans_dct(es_en_trans)
+fr_en_dct = trans_dct(fr_en_trans)
+
+## Function for adding Concreteness, Frequency and Distance to ftr vectors
+def ftrs_3(data, trans_dct, track): ## remove dct_brys and dct_sp_freq from input, these are constant    
+    for instance in data:
+        if instance[1] in trans_dct:
+            L2_word = instance[1]
+            L1_word = str(trans_dct[L2_word]).lower()         
+            if track == "en_es":                                ## NB: es = L1, en = L2, Spanish learning English. 
+                if L1_word in dct_Spanish_freq and L2_word in dct_Brysbaert:
+                    dis=lev.distance((L1_word), str(L2_word))
+                    instance[7]=dct_Spanish_freq[L1_word]
+                    instance[8]=dis
+                    instance[9]=dct_Brysbaert[L2_word]
+    
+            if track == "es_en":
+                if instance[3] == 'VERB' and len(L1_word.split()) > 1:
+                    L1_word = L1_word.split()[-1]                  ## spanish verbs are often translated into english with pronouns, this takes only the verb (the last word) 
+
+                if L1_word in dct_Brysbaert and L1_word in dct_Enfreq:
+                    dis=lev.distance((L1_word), str(L2_word))         
+                    instance[7]=dct_Enfreq[L1_word]
+                    instance[8]=dis
+                    instance[9]=dct_Brysbaert[L1_word]
+                    
+            if track == "fr_en":            
+                if L1_word in dct_Brysbaert and L1_word in dct_Enfreq:
+                    dis=lev.distance((L1_word), str(L2_word))          
+                    instance[7]=dct_Enfreq[L1_word]
+                    instance[8]=dis
+                    instance[9]=dct_Brysbaert[L1_word]
+    return data
+
+## Add Concreteness, Frequency and Distance to ftr vectors
+en_es_3 = ftrs_3(en_es_2, en_es_dct, 'en_es')
+es_en_3 = ftrs_3(es_en_2, es_en_dct, 'es_en')
+fr_en_3 = ftrs_3(fr_en_2, fr_en_dct, 'fr_en')
+
+## Unfamiliar Sound Spellings
+En_Es_Unf = ['ee', 'augh', 'tt', 'ow', 'oa', 'oo', 'ou']
+Fr_En_Unf = ['ui', 'eu', 'un', 'in', 'ai', 'en', 'ei', 'oeu']
+Es_En_Unf = ['ll', 'j', 'x']
+
+## Dictionary with Word as Key, and Unfamiliar sound (1/0) as value
+## Include unfamiliar sound ftr in final ftr vectors
+def ftr_unf_sound(data, unique_words, unf_sound_list):
+    dct_ftr = dict()
+    
+    for word in unique_words:
+        unf_sound = []
+        for spelling in unf_sound_list:
+            unf_sound.append(spelling in word)
+            if any(unf_sound):
+                dct_ftr[word] = 1
+            else:
+                dct_ftr[word] = 0
+    for instance in data:
+        instance[10] = dct_ftr[instance[1]]   
+    return dct_ftr, data
+
+## Define dictionaries, and complete ftrs 
+en_es_dct_unf, en_es_compl = ftr_unf_sound(en_es_3, unique_words_en, En_Es_Unf)
+es_en_dct_unf, es_en_compl = ftr_unf_sound(es_en_3, unique_words_es, Es_En_Unf)
+fr_en_dct_unf, fr_en_compl = ftr_unf_sound(fr_en_3, unique_words_fr, Fr_En_Unf)
+
+## Concatenate all tracks
+ftrs_compl = np.concatenate((en_es_compl, es_en_compl, fr_en_compl), axis = 0)
+
+## Create dictionary with number of observation per user. 
+def user_counts(data):
+    user_dct = dict()
+    users_lst = list(data[:,0])
+    unique_users = (set(users_lst))
+    for user in unique_users:
+        user_dct[user] = users_lst.count(user) 
+    return user_dct
+
+user_counts = user_counts(ftrs_compl)
+
+## Function to mark users outside of 10th and 90th percentile
+def drop_users(user_counts, data):    
+    drop_users = []
+    dropped_instances = 0
+    
+    for user in user_counts.keys():
+        if user_counts[user]<200 or user_counts[user]>630:
+            drop_users.append(user)
+            
+    for row in data:
+        if row[0] in drop_users:
+            row[0] = None
+            dropped_instances +=1
+    return drop_users, data, dropped_instances
+
+drop_users, ftrs_compl, dropped_instances = drop_users(user_counts, ftrs_compl)
+
+## Turn into dataframe, drop words that have no frequency and/or concreteness scores, and marked users
+df_ftrs=pd.DataFrame(ftrs_compl)
+df_ftrs=df_ftrs.dropna()
+df_ftrs.columns=['User', 'Word', 'Task_Format', 'PoS', 'Word_length', 'Track', 'Word_exp', 'Frequency', 'Distance', 'Concreteness', 'Unfamiliar_Sound', 'Nr_Mistakes']
+df_ftrs.to_csv("ftrs.csv")
+
---
+++ Thesis_final.R
@@ -0,0 +1,280 @@
+library(MASS)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(readr)
+library(extraDistr)
+library(ggplot2)
+library(brms)
+library(rstan)
+rstan_options(auto_write = TRUE)
+
+library(bayesplot)
+library(tictoc)
+library(gridExtra)
+library(parallel)
+library(DataCombine)
+library(MCMCvis)
+
+set.seed(333)
+ftrs <- read_csv("CSAI/Thesis/ftrs (1).csv")
+
+## Distribution of Mistakes 
+boxplot_mis = boxplot(ftrs$Nr_Mistakes, main='Boxplot: Nr of Mistakes')
+distr_mis = hist(ftrs$Nr_Mistakes) 
+
+## Group PoS Tags 
+# count occurences of every tag, 
+levels(ftrs$PoS)
+fill <- NULL
+for(i in 1:15){
+  data = (filter(ftrs, PoS==levels(ftrs$PoS)[i]))
+  fill[i] <- nrow(data)
+}
+
+# Group PoS tags with less than 100.000, under others. Final categories: Adj, Det, Noun, Pron, Propn, Verb, and Others.
+to_replace <- c("ADP", "ADV", "AUX", "CONJ", "INTJ", "NUM", "PART", "SCONJ", "X")
+for (tag in to_replace){
+  ftrs[ftrs==tag]<-"OTHER"
+}
+ftrs <- filter(ftrs, PoS!='OTHER')
+
+## Log transform freq and exp and scale ftrs
+# Numerical: 
+ftrs$Frequency <- log(ftrs$Frequency)
+ftrs$Word_exp <- log(ftrs$Word_exp)
+ftrs[c(6, 8, 9, 10, 11)] <- scale(ftrs[c(6, 8, 9, 10, 11)])
+
+##  Contrast Coding Categorical ftrs 
+ftrs$Task_Format <- as.factor(ftrs$Task_Format)
+ftrs$PoS <- as.factor(ftrs$PoS)
+ftrs$Track <- as.factor(ftrs$Track)
+ftrs$Unfamiliar_Sound <- as.factor(ftrs$Unfamiliar_Sound)
+
+# Sum contrast coding, and treatment for unfamiliar sound
+contrasts(ftrs$Task_Format) <- contr.sum(3)
+contrasts(ftrs$PoS) <- contr.sum(6)
+contrasts(ftrs$Track) <- contr.sum(3)
+contrasts(ftrs$Unfamiliar_Sound) <- contr.treatment(2)  
+
+
+## Summary Statistics 
+es_data <- filter(ftrs, Track=='es_en')
+en_data <- filter(ftrs, Track=='en_es')
+fr_data <- filter(ftrs, Track=='fr_en')
+
+# number of zero mistakes
+zero_mis <- filter(ftrs, Nr_Mistakes == 0)
+# 1.004.753
+
+#Five or less mistakes:
+five_or_less <- filter(ftrs, Nr_Mistakes < 5)
+# 1.290.460
+
+en_users = unique(en_data$User)
+es_users = unique(es_data$User)
+fr_users = unique(fr_data$User)
+
+en_words = unique(en_data$Word)
+es_words = unique(es_data$Word)
+fr_words = unique(fr_data$Word)
+
+length(en_users)
+length(en_words)
+length(es_users)
+length(es_words)
+length(fr_users)
+length(fr_words)
+
+listen <- filter(ftrs, Task_Format == "listen")    ## A tibble: 421,999 x 13
+tap <- filter(ftrs, Task_Format == "reverse_tap")  ## A tibble: 396,796 x 13
+translate <- filter(ftrs, Task_Format =="reverse_translate") # A tibble: 476,098 x 13
+
+## Collinearity Check
+freq_len <- cbind(ftrs$Frequency, ftrs$Word_length)
+freq_len_plot <- pairs(freq_len)
+
+## Analysis
+# Set Priors
+get_prior(formula = Nr_Mistakes ~ Task_Format + PoS + Word_length + Track + Word_exp + Concreteness + Frequency + Distance + 
+            Unfamiliar_Sound + (1 + Task_Format | Word) + (Task_Format + Concreteness + Frequency + Distance + Unfamiliar_Sound + Word_length | User), 
+          data = ftrs, family = zero_inflated_poisson())
+
+
+priors <- c(set_prior("normal(0, 5)", class = "Intercept"), 
+            set_prior("normal(0, 1)", class = "b"),
+            set_prior("normal(0, 1)", class = "sd"))
+
+
+## Train the model on 2 different stratified samples (20 users per track), total of approximately 14000 observations per sample
+## to check if coefficient values are relatively consistent across samples, indicating that samples are statistically representative
+Summs_1 <- NULL
+for(i in 1:2){
+  sample_en <- sample(en_users, 20)
+  sample_es <- sample(es_users, 20)
+  sample_fr <- sample(fr_users, 20)
+  sample_users <- c(sample_en, sample_es, sample_fr)
+  strat_sample <- filter(ftrs, User %in% sample_users)
+  SS <- brm(formula = Nr_Mistakes ~  Task_Format + PoS + Word_length + Track + Word_exp + Concreteness + Frequency + Distance + Unfamiliar_Sound +
+              + (1 | Word) + (1| User),
+            data = strat_sample, family = zero_inflated_poisson(), prior = priors,
+            iter = 3000, chains = 4, cores = 4, control = list(adapt_delta = 0.99, max_treedepth = 10))
+  Summs_1[[i]] <- summary(SS)
+}
+
+Summs_2 <- NULL
+for(i in 1:2){
+  sample_en <- sample(en_users, 25)
+  sample_es <- sample(es_users, 25)
+  sample_fr <- sample(fr_users, 25)
+  sample_users <- c(sample_en, sample_es, sample_fr)
+  strat_sample <- filter(ftrs, User %in% sample_users)
+  SS <- brm(formula = Nr_Mistakes ~  Task_Format + PoS + Word_length + Track + Word_exp + Concreteness + Frequency + Distance + Unfamiliar_Sound +
+              + (1 | Word) + (1| User),
+            data = strat_sample, family = zero_inflated_poisson(), prior = priors,
+            iter = 3000, chains = 4, cores = 4, control = list(adapt_delta = 0.99, max_treedepth = 10))
+  Summs_2[[i]] <- summary(SS)
+}
+
+# Sample for training and comparing models 
+set.seed(248)
+sample_en <- sample(en_users, 20)
+sample_es <- sample(es_users, 20)
+sample_fr <- sample(fr_users, 20)
+sample_users <- c(sample_en, sample_es, sample_fr)
+strat_sample <- filter(ftrs, User %in% sample_users)
+
+# Baseline model
+MC1 <- brm(formula = Nr_Mistakes ~ Task_Format + PoS + Track + Word_exp + Concreteness + Frequency + Distance + Word_length +  Unfamiliar_Sound +
+             (1 | Word) + (1 | User),
+           data = strat_sample, family = zero_inflated_poisson(), prior = priors,
+           iter = 2500, chains = 4, cores = 4, control = list(adapt_delta = 0.99, max_treedepth = 10))
+
+# check model/likelihood fit
+pp_check(MC1, nsamples = 200)
+
+# Feature Selection
+MC2 <- brm(formula = Nr_Mistakes ~  Task_Format + PoS + Word_exp + Concreteness  + Distance  + Word_length +
+             (1 | Word) + (1 | User),
+           data = strat_sample, family = zero_inflated_poisson(), prior = priors,
+           iter = 2500, chains = 4, cores = 4, control = list(adapt_delta = 0.99, max_treedepth = 10))
+
+# Model comparison 
+MC1 <- add_criterion(MC1,'waic')
+MC2 <- add_criterion(MC2, 'waic')
+comp <- loo_compare(MC1, MC2, criterion="waic")
+comp
+
+
+# Add Interaction Terms
+IM1 <- brm(formula = Nr_Mistakes ~  Task_Format  + PoS  + Word_exp + Concreteness + Distance + Word_length +
+               PoS: Task_Format + Concreteness:Task_Format+ Distance:Task_Format + Word_length:Task_Format + 
+               (1 | Word) + (1 | User),
+               data = strat_sample, family = zero_inflated_poisson(), prior = priors,
+               iter = 2500, chains = 4, cores = 4, control = list(adapt_delta = 0.99, max_treedepth = 10))
+
+IM1 <- add_criterion(IM1, "waic")
+comp_2 <- loo_compare(MC2, IM1, criterion="waic")
+
+
+
+# Add Random Slopes
+priors <- c(set_prior("normal(0, 5)", class = "Intercept"), 
+            set_prior("normal(0, 1)", class = "b"),
+            set_prior("normal(0, 1)", class = "sd"),
+            set_prior("lkj(2)", class = "cor")) 
+
+RS2 <- brm(formula = Nr_Mistakes ~  Task_Format  + PoS  + Word_exp + Concreteness + Distance + Word_length 
+           + Task_Format:PoS + Task_Format:Concreteness + Task_Format:Distance + Task_Format:Word_length + 
+            (1 | Word) + (Task_Format + PoS + Concreteness + Distance + Word_length + Task_Format:PoS +
+            Task_Format:Concreteness + Task_Format:Distance + Task_Format:Word_length | User),
+           data = strat_sample, family = zero_inflated_poisson(), prior = priors, inits = 0,
+           iter = 2500, chains = 4, cores = 4, control = list(adapt_delta = 0.99, max_treedepth = 12))
+
+RS2 <- add_criterion(RS2, "waic")
+comp_3 <- loo_compare(MC2, IM1, RS1, criterion="waic")
+
+
+## New stratified sample, to ensure best model is still best model on new sample and not 'overfitted'
+sample_en_2 <- sample(en_users, 20)
+sample_es_2 <- sample(es_users, 20)
+sample_fr_2 <- sample(fr_users, 20)
+sample_users_2 <- c(sample_en_2, sample_es_2, sample_fr_2)
+strat_sample_2 <- filter(ftrs, User %in% sample_users_2)
+
+FMC1 <- brm(formula = Nr_Mistakes ~  Task_Format  + PoS  + Word_exp + Concreteness + Frequency + Distance + Word_length +
+              (1 | Word) + (1 | User),
+            data = strat_sample_2, family = zero_inflated_poisson(), prior = priors,
+            iter = 2500, chains = 4, cores = 4, control = list(adapt_delta = 0.99, max_treedepth = 10))
+
+FMC2 <- brm(formula = Nr_Mistakes ~  Task_Format  + PoS  + Word_exp + Concreteness + Distance + Word_length +
+             PoS:Task_Format: Concreteness:Task_Format + Distance:Task_Format + Word_length:Task_Format + 
+              (1 | Word) + (1 | User),
+            data = strat_sample_2, family = zero_inflated_poisson(), prior = priors,
+            iter = 2500, chains = 4, cores = 4, control = list(adapt_delta = 0.99, max_treedepth = 10))
+
+FMC3 <- brm(formula = Nr_Mistakes ~  Task_Format  + PoS  + Word_exp + Concreteness + Distance + Word_length +
+              + Task_Format:PoS + Task_Format:Concreteness + Task_Format:Distance + Task_Format:Word_length + 
+              (1 | Word) + (Task_Format + PoS + Frequency + Concreteness + Distance +
+              Task_Format:PoS + Task_Format:Concreteness + Task_Format:Distance + Task_Format:Word_length | User),
+            data = strat_sample_2, family = zero_inflated_poisson(), prior = priors,
+            iter = 2500, chains = 4, cores = 4, inits = 0, control = list(adapt_delta = 0.99, max_treedepth = 12))
+
+FMC1 <- add_criterion(FMC1, "waic")
+FMC2 <- add_criterion(FMC2, "waic")
+FMC3 <- add_criterion(FMC3, "waic")
+
+final_comp <- loo_compare(FMC1, FMC2, FMC3, criterion="waic")
+final_comp
+
+## Plots for Visualizing Results
+
+# Coefficents and CI for Baseline model
+BL_coef <- MCMCplot(MC1, params = c('Intercept', 'Task_Format1', 'Task_Format2', 'PoS1', 'PoS2', 'PoS3', 'PoS4', 'PoS5',  
+                                    'Track1', 'Track2', 'Word_exp', 'Concreteness', 'Frequency', 'Distance', 'Word_length', 'Unfamiliar_Sound2'),
+                    labels = c('Intercept', 'Task:Listen', 'Task:Tap', 'PoS:Adj', 'PoS:Det', 'PoS:Noun', 'PoS:Pron', 'PoS:PropN', 
+                               'Track:En_Es', 'Track:Es_En', 'Word_exp', 'Concreteness', 'Frequency', 'Distance', 'Word_length', 'Unfamiliar_Sound') )
+
+# Plots conditional effects of the interaction terms 
+int_effects <- plot(conditional_effects(IM3))
+
+
+## Posterior Distributions of maximal model
+
+# Population-level effects
+mcmc_plot(RS1, pars = c("^b_"), type="hist")
+
+# Group-level effects
+mcmc_plot(RS1, pars = c("sd_"), type="hist")
+
+# Seperate Random intercept User and Word
+sd_random_int <- mcmc_plot(RS1, pars = c("sd_User__I", "sd_Word__I"), type="hist")
+
+
+
+
+
+
+sd_user_plot1 <- mcmc_plot(RS1, pars = c('sd_User__P', 'sd_User__C', 'sd_User__D', 'sd_User__W'), type="hist")
+sd_user_plot2 <- mcmc_plot(RS1, pars = c("sd_User__Task_Format1"), type="hist")
+sd_user_plot3 <- mcmc_plot(RS1, pars = c("sd_User__Task_Format2"), type="hist")
+sd_user_plot4 <- mcmc_plot(RS1, pars = c("sd_User__Task_Format1:PoS1", "sd_User__Task_Format2:PoS1",
+                                         "sd_User__Task_Format1:PoS2", "sd_User__Task_Format2:PoS2", 
+                                         "sd_User__Task_Format1:PoS3", "sd_User__Task_Format2:PoS3",
+                                         "sd_User__Task_Format1:PoS4", "sd_User__Task_Format2:PoS4"), type="hist")
+
+
+sd_user_plot5 <- mcmc_plot(RS1, pars = c("sd_User__Task_Format1:PoS5", "sd_User__Task_Format2:PoS5",
+                                         "sd_User__Task_Format1:C", "sd_User__Task_Format2:C",
+                                         "sd_User__Task_Format1:D", "sd_User__Task_Format2:D", 
+                                         "sd_User__Task_Format1:W", "sd_User__Task_Format2:W"), type="hist")
+
+
+
+sd_rs_maineffects <- mcmc_plot(RS1, pars = c("sd_User__I", "sd_Word__I",
+                                             'sd_User__P', 'sd_User__C', 'sd_User__D', 'sd_User__W'), type="hist")
+
+
+
+
+
